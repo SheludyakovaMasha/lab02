3f4f85ea6bf4129562952cea95708837b8da6640
SheludyakovaMasha <sheludyakova_masha@mail.ru>
2023-05-20 22:39:07 +0300

    changed code style in cpp file

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
- 
 
-int main(int argc, char** argv) {
-   string name; //User name
-   std::cin >> name; // Input user name
-   std::cout << "Hello world from " << name << std::endl;
+int
+main(int argc, char** argv)
+{
+  string name;                                      // Name of @user
+  std::cin >> name;                                 // Input name of @user
+  std::cout << "Hello world from " << name << endl; // Output name of @user
 }
